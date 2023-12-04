cdd0aa40818d8ceb2b3b5cc341685ee91dd019c3
Pilmo <pilmokim99@gmail.com>
2023-12-04 19:41:06 +0900

    add feature to find out web server's pid

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,28 @@
 use actix_web::{web, App, HttpServer, Responder};
+use std::sync::atomic::{AtomicUsize, Ordering};
+use std::sync::Arc;
 
-async fn greet() -> impl Responder {
-    "Hello, world!"
+async fn greet(counter: web::Data<Arc<AtomicUsize>>) -> impl Responder {
+    let count = counter.fetch_add(1, Ordering::SeqCst);
+
+    if count >= 5 {
+        // Logic to stop the server
+        println!("Received 5 GET requests, shutting down.");
+        std::process::exit(0);
+    }
+
+    format!("Hello! You are visitor number {}", count + 1)
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+    let pid = std::process::id();
+    println!("The PID of this process is: {}", pid);
+    let counter = Arc::new(AtomicUsize::new(0));
+
+    HttpServer::new(move || {
         App::new()
+            .app_data(web::Data::new(counter.clone()))
             .route("/", web::get().to(greet))
     })
     .bind("127.0.0.1:8080")?
